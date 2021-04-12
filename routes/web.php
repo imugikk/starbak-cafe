a0432f7d77f0f02c6c71154ce6439cd7cd8969f3
pridyarahma <82138211+pridyarahma@users.noreply.github.com>
2021-04-12 20:44:27 +0700

    create and delet data

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/merchandise', 'MerchandiseController@index');
+Route::post('/merchandise/store', 'MerchandiseController@store');
+Route::get('/merchandise/delete/{id}', 'MerchandiseController@delete');
+
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
