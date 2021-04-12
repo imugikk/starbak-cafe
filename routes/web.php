0a16333ec4250dba15effe1a7f21d857ce60ee63
sara flo <sara.telaumbanua@gmail.com>
2021-04-12 20:06:47 +0700

    commit create and delete data

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/staff', 'StaffController@index');
+Route::post('/staff/store', 'StaffController@store');
+Route::get('staff/delete/{id}', 'StaffController@delete');
